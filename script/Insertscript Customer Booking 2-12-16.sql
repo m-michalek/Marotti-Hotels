485f47447b01008b1cec2c338584a85b8c663039
O.Mrost <olgamrost@gmail.com>
2016-12-02 20:01:11 +0100

    Added 2 scripts: to create CUSTOMER and BOOKING tables, and to insert several customers and several bookings into the tables

---
+++ script/Insertscript Customer Booking 2-12-16.sql
@@ -0,0 +1,17 @@
+insert into customer (Customer_ID, customer_first_name, customer_last_name, account_number, birthdate)
+  values(1, 'James', 'Bond', 123456789, '01-01-1970');
+  
+  insert into customer (Customer_ID, customer_first_name, customer_last_name, account_number, birthdate)
+  values(2, 'John', 'Doe', 987654321, '18-07-1990');
+  
+  insert into customer (Customer_ID, customer_first_name, customer_last_name, account_number, birthdate)
+  values(3, 'Jane', 'Doe', 347282923, '24-08-1995');
+  
+   insert into customer (Customer_ID, customer_first_name, customer_last_name, account_number, birthdate)
+  values(4, 'Jonny', 'Doe', 362718564, '09-02-2010');
+  
+   insert into booking (booking_id, customer_id, check_in_date, check_out_date, bookingcode)
+  values(100, 1, '1.1.2016', '3.1.2016', 's3434dsd');
+  
+   insert into booking (booking_id, customer_id, check_in_date, check_out_date, bookingcode)
+  values(101, 1, '21.5.2016', '26.5.2016', 's343se2sd');
